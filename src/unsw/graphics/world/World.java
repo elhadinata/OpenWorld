f90514e08c3f882bc1a2d79a6567fba4512c0373
Elena Hadinata <elhadinata@Elenas-MacBook-Pro.local>
2019-07-17 22:26:30 +1000

    still dodgy

--- src/unsw/graphics/world/World.java
+++ src/unsw/graphics/world/World.java
@@ -116,9 +116,9 @@ public class World extends Application3D implements KeyListener{
 
             // Set the material properties
             Shader.setColor(gl, "ambientCoeff", Color.WHITE);
-            Shader.setColor(gl, "diffuseCoeff", new Color(0.5f, 0.5f, 0.5f));
-            Shader.setColor(gl, "specularCoeff", new Color(0.8f, 0.8f, 0.8f));
-            Shader.setFloat(gl, "phongExp", 16f);
+            Shader.setColor(gl, "diffuseCoeff", new Color(1f, 1f, 1f));
+            Shader.setColor(gl, "specularCoeff", new Color(0.5f, 0.5f, 0.5f));
+            Shader.setFloat(gl, "phongExp", 128f);
         }
         
         Shader.setPenColor(gl, Color.WHITE);
